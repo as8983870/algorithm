3e00d9ae0493f1c8183e52aa92dbb439cdfd589e
胡志恒 <zhiheng.hu@chinaredstar.com>
2019-07-17 15:09:48 +0800

    commit

---
+++ src/main/java/com/algorithm/alicloudtest/Test.java
@@ -0,0 +1,36 @@
+package com.algorithm.alicloudtest;
+
+/**
+ * Created by h on 2019/7/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        /*int i = 1;
+        int j = i ++;
+        if((i==(++j))&&((i++)==j)){
+            i+=j;
+        }
+        System.out.println("i="+i);*/
+
+        /*int num = 2147483647;
+        long temp = num + 2L;
+        System.out.println(num);*/
+
+        /*int num = 2147483647;
+        num += 2;
+        System.out.println(num);*/
+
+        /*int sum = 0;
+        int x = 10;
+        while (x>0){
+            sum +=x;
+        }
+        System.out.println(sum);*/
+
+        /*int x = 10;
+        double y =20.2;
+        long z = 10L;
+        String str = "" + x + y * z;
+        System.out.println(str);*/
+    }
+}
